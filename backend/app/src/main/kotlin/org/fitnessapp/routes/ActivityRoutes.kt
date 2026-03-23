86c716524aec99b51c05864314e4c45efaeacae8
IvanParvanovski <ivan.parvanovski347@gmail.com>
2026-03-23 11:01:35 +0000

    Refactored activity routes

--- backend/app/src/main/kotlin/org/fitnessapp/routes/ActivityRoutes.kt
+++ backend/app/src/main/kotlin/org/fitnessapp/routes/ActivityRoutes.kt
@@ -9,11 +9,51 @@ import io.ktor.http.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.statements.InsertStatement
 
 import org.fitnessapp.models.Activity
 import org.fitnessapp.models.ActivityDTO
+import org.fitnessapp.models.CreateActivityRequest
 
 import java.time.LocalDate
+
+private fun ResultRow.toActivityDTO() = ActivityDTO(
+    id = this[Activity.id],
+    date = this[Activity.date].toString(),
+    profileId = this[Activity.profileId],
+    exerciseId = this[Activity.exerciseId]
+)
+
+private fun CreateActivityRequest.toActivityDTO(id: Long) = ActivityDTO(
+    id = id,
+    date = date,
+    profileId = profileId,
+    exerciseId = exerciseId
+)
+
+private fun createActivity(
+    builder: InsertStatement<*>,
+    request: CreateActivityRequest
+) {
+    builder[Activity.date] = LocalDate.parse(request.date)
+    builder[Activity.profileId] = request.profileId
+    builder[Activity.exerciseId] = request.exerciseId
+}
+
+private fun findActivitiesByProfile(
+    profileId: Long,
+    date: java.time.LocalDate?
+): List<ActivityDTO> = transaction {
+    Activity.selectAll().where {
+        if (date != null) {
+            (Activity.profileId eq profileId) and (Activity.date eq date)
+        } else {
+            Activity.profileId eq profileId
+        }
+    }.map {
+        it.toActivityDTO()
+    }
+}
 
 fun Route.activityRoutes() { 
     route("/activities") {
@@ -29,38 +69,24 @@ fun Route.activityRoutes() {
                 }
             }
 
-            val activities = transaction {
-                Activity.selectAll().where {
-                    if (date != null) {
-                        (Activity.profileId eq profileId) and (Activity.date eq date)
-                    } else {
-                        Activity.profileId eq profileId
-                    }
-                }.map {
-                    ActivityDTO(
-                        id = it[Activity.id],
-                        date = it[Activity.date].toString(),
-                        profileId = it[Activity.profileId],
-                        exerciseId = it[Activity.exerciseId]
-                    )
-                }
-            }
+            val activities = findActivitiesByProfile(profileId, date)
 
             call.respond(HttpStatusCode.OK, activities)
         }
 
         post {
-            val activity = call.receive<ActivityDTO>()
+            val activity = call.receive<CreateActivityRequest>()
 
             val createdActivityId = transaction { 
-                Activity.insert {
-                    it[date] = LocalDate.parse(activity.date)
-                    it[profileId] = activity.profileId
-                    it[exerciseId] = activity.exerciseId
+                Activity.insert { builder ->
+                    createActivity(builder, activity)
                 } get Activity.id
             }
 
-            call.respond(HttpStatusCode.Created, activity.copy(id=createdActivityId))
+            call.respond(
+                HttpStatusCode.Created,
+                activity.toActivityDTO(createdActivityId)
+            )
         }
 
         delete("/{id}") {

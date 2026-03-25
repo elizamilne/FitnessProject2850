0d7afa2c0c9dd116d3ac0a8dc32e37350a5eff27
IvanParvanovski <ivan.parvanovski347@gmail.com>
2026-03-25 01:17:26 +0000

    Implemented Training Overview layout

--- frontend/src/pages/TrainingPage.jsx
+++ frontend/src/pages/TrainingPage.jsx
@@ -1,5 +1,6 @@
 import TrainingCalendar from "../components/TrainingPage/TrainingCalendar";
 import TrainingContentList from "../components/TrainingPage/TrainingContentList";
+import TrainingOverviewRow from "../components/TrainingPage/TrainingOverviewRow";
 import TrainingProgramSelector from "../components/TrainingPage/TrainingProgramSelector";
 import TrainingStatistics from "../components/TrainingPage/TrainingStatistics";
 
@@ -18,9 +19,16 @@ const TrainingPage = () => {
         <TrainingProgramSelector></TrainingProgramSelector>
         <TrainingContentList></TrainingContentList>
 
-        <div className="mt-5 bt-5"></div>
+        <div className="mt-12 bt-12"></div>
 
         <TrainingStatistics></TrainingStatistics>
+
+        <div className="mt-12 bt-12"></div>
+
+        <TrainingOverviewRow></TrainingOverviewRow>
+
+        <div className="mt-12 bt-12"></div>
+
       </div>
     </div>
   );

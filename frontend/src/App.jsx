7933c64febd6243560687d32ec3304ffca9df665
IvanParvanovski <ivan.parvanovski347@gmail.com>
2026-04-18 23:10:51 +0300

    Fixed import bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomeScreen from "./components/HomeScreen";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+import HomeScreen from "./features/home/HomeScreen";
+import Login from "./features/auth/Login";
+import SignUp from "./components/Signup";
 import Questions from "./components/Questions";
+
+import TrainingPage from "./features/training/TrainingPage";
+import ActivitiesPage from "./features/activities/ActivitiesPage";
 
 function App() {
   return (
@@ -15,7 +18,7 @@ function App() {
         <Route path="/activities-page" element={<ActivitiesPage/>} />
 
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/questions" element={<Questions />} />
       </Routes>
     </BrowserRouter>

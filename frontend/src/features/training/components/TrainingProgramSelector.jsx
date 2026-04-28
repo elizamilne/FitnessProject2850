453a95aac30b1dca83767b03671d936b9a6b5065
IvanParvanovski <ivan.parvanovski347@gmail.com>
2026-04-28 15:25:05 +0100

    Improved program selection design consistency

--- frontend/src/features/training/components/TrainingProgramSelector.jsx
+++ frontend/src/features/training/components/TrainingProgramSelector.jsx
@@ -100,7 +100,7 @@ const TrainingProgramSelector = ({
 
       {/* row */}
       <div className="flex items-center gap-3">
-        {/* LEFT */}
+        {/* Left */}
         <button
           onClick={() => shiftPrograms(-1)}
           disabled={isStart}
@@ -132,7 +132,7 @@ const TrainingProgramSelector = ({
           <ChevronLeft size={20} />
         </button>
 
-        {/* PROGRAMS */}
+        {/* Programs */}
         <div ref={containerRef} className="flex flex-1 gap-2 overflow-hidden">
           {visiblePrograms.map((program) => {
             const isSelected = selectedProgram === program;
@@ -145,20 +145,20 @@ const TrainingProgramSelector = ({
                   onProgramChange?.(program);
                 }}
                 className={`
-          flex-1 min-w-0 px-5 py-3 text-center cursor-pointer
-          rounded-xl transition-all duration-200
+                  flex-1 min-w-0 px-5 py-3 text-center cursor-pointer
+                  rounded-xl transition-all duration-200
 
-          ${
-            isSelected
-              ? `
-                bg-indigo-100 text-indigo-600
-              `
-              : `
-                bg-white text-gray-500
-                hover:bg-gray-100 hover:text-gray-600
-              `
-          }
-        `}
+                  ${
+                    isSelected
+                      ? `
+                        bg-indigo-100 text-indigo-600
+                      `
+                      : `
+                        bg-white text-gray-500
+                        hover:bg-gray-100 hover:text-gray-600
+                      `
+                  }
+                `}
               >
                 <span className="block truncate text-base font-semibold">
                   {program.title}
@@ -168,7 +168,7 @@ const TrainingProgramSelector = ({
           })}
         </div>
 
-        {/* RIGHT */}
+        {/* Right */}
         <button
           onClick={() => shiftPrograms(1)}
           disabled={isEnd}

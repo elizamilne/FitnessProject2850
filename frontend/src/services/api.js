5984c81608559679f49660646a0a47fb646ac3a1
IvanParvanovski <ivan.parvanovski347@gmail.com>
2026-05-07 17:44:35 +0100

    Added an .env file for codespaces

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+
 export const api = axios.create({
-    baseURL: "http://localhost:8080",
-    headers: {
-        "Content-Type": "application/json"
-    },
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use((config) => {
